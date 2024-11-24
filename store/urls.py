8ec0e8617d191e9b573949ed7353db77c7e84984
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-11-25 00:46:44 +0330

    urls cart and viewset for cart

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,10 @@ from . import views
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('categories', views.CategoryViewSet, basename='category')
+router.register('carts', views.CartViewSet, basename='cart')
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('comments', views.CommentViewSet, basename='product-comments')
 
-urlpatterns = router.urls 
+cart_items_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_items_router.register('items', views.CartItemViewSet, basename='cart-items')
+urlpatterns = router.urls + products_router.urls + cart_items_router.urls
--- store/views.py
+++ store/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response # type: ignore
-from .models import Product, Customer, OrderItem, Order, Category, Customer, Comment
+from rest_framework.response import Response
+
+from store.paginations import DefaultPagination
+from .models import CartItem, Product, Customer, OrderItem, Order, Category, Customer, Comment, Cart
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count, Avg, F, Func, Value
-from .serializers import ProductSerializer, CategorySerializer
+from .serializers import AddCartItemSerializer, CartItemSerializer, ProductSerializer, CategorySerializer, CommentSerializer, CartSerializer, UpdateCartItemSerializer
 from rest_framework import status # type: ignore
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet # type: ignore
 from .filters import ProductFilter
+
+
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.prefetch_related('products').all()
@@ -26,6 +32,7 @@ class ProductViewSet(ModelViewSet):
     ordering_fields = ['name', 'unit_price', 'inventory']
     search_fields = ['name', 'category__title']
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     def destroy(self, request, pk):
         product = get_object_or_404(
             Product.objects.select_related('category'),
@@ -36,3 +43,37 @@ class ProductViewSet(ModelViewSet):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    def get_queryset(self):
+        product_pk = self.kwargs['product_pk']
+        return Comment.objects.filter(product_id=product_pk).all()
+    
+    def get_serializer_context(self):
+        return {'product_pk': self.kwargs['product_pk']}
+    
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_queryset(self):
+        cart_pk = self.kwargs['cart_pk']
+        return CartItem.objects.select_related('product').filter(cart_id=cart_pk).all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_pk': self.kwargs['cart_pk']}
+
+
+class CartViewSet(CreateModelMixin,
+                   RetrieveModelMixin,
+                   DestroyModelMixin,
+                   GenericViewSet):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.prefetch_related('items__product').all()
